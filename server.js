e952950f09fa76f8eeffbf06b77b4f8b4509331d
Subramaniyam2003 <valarshan2000@gmail.com>
2025-05-07 10:51:50 +0530

    final commit

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const mongoose = require("mongoose");
 const PORT=8000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
